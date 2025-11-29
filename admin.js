3433b0557e0dd797d04b73a65c832e5138408d5d
Ganta-sireesha21 <gantasireesha1998@gmail.com>
2025-11-29 20:33:42 +0530

    Create a fleet managemet app

---
+++ admin.js
@@ -0,0 +1,28 @@
+let vehicles = [];
+const form = document.getElementById("vehicleForm");
+const vehiclelist = document.getElementById("vehiclelist");
+const filterType = document.getElementById("flterType");
+form.addEventListener("submit", function (e)) {
+    e.preventDefault();
+    const name = document.getElementById("name").value.trim();
+    const type = document.getElementById("type").value.trim();
+    const capacity = document.getElementById("capacity").value;
+    const availability = document.getElementById("availability").value.trim();
+if (!name || !type || !capacity) {
+    alert("please fill all field");
+    return;
+}
+const newVehicle = {
+    id: Date.now(),
+    name,
+    type,
+    capacity,
+    availability
+}
+vehicles.push(newVehicle);
+displayVehicles(vehicles);
+form.reset();
+};
+ function displayVehicles(data) {
+    
+ }
